088f9d831a6321af0eaff2f1918fa8d6743d2778
tom-elmore <tom.elmore@just-eat.com>
2019-02-21 11:20:09 +0000

    empty project for sns monitor

---
+++ SnsMonitor/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Amazon.SimpleNotificationService;
+using Amazon.SimpleNotificationService.Model;
+
+namespace SnsMonitor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AmazonSimpleNotificationServiceClient snsClient = new AmazonSimpleNotificationServiceClient(Amazon.RegionEndpoint.EUWest1);
+
+            //SubscribeRequest subRequest = new SubscribeRequest("", );
+        }
+    }
+}
